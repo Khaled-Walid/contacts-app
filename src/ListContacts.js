0fe9624e9f01e6f379ef93d6952e66c6de42e2e8
Khaled-Walid <justkhwf@outlook.com>
2021-11-14 19:39:25 +0200

    Move default contacts into App component as state

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 class ListContacts extends Component {
   render() {
     return (
-      <ol className='contact-list'>
+      <ol className="contact-list">
         {this.props.contacts.map((contact) => (
-          <li key={contact.id} className='contact-list-item'>
+          <li key={contact.id} className="contact-list-item">
             <div
-              className='contact-avatar'
+              className="contact-avatar"
               style={{
-                backgroundImage: `url(${contact.avatarURL})`
+                backgroundImage: `url(${contact.avatarURL})`,
               }}
-            ></div>
-            <div className='contact-details'>
+            />
+            <div className="contact-details">
               <p>{contact.name}</p>
               <p>{contact.handle}</p>
             </div>
-            <button className='contact-remove'>
-              Remove
-            </button>
+            <button className="contact-remove">Remove</button>
           </li>
         ))}
       </ol>
-	
+    );
+  }
+}
+export default ListContacts;
