2536266e960bbe2865cd3877795abe2b8d1b40d6
Khaled-Walid <justkhwf@outlook.com>
2021-11-14 20:11:52 +0200

    Adds 'Now showing' section to clear search query

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -13,17 +13,18 @@ class ListContacts extends Component {
       query: query.trim(),
     }));
   };
+  clearQuery = () => {
+    this.updateQuery("");
+  };
   render() {
     const { query } = this.state;
     const { contacts, onDeleteContact } = this.props;
-
     const showingContacts =
       query === ""
         ? contacts
         : contacts.filter((c) =>
             c.name.toLowerCase().includes(query.toLowerCase())
           );
-
     return (
       <div className="list-contacts">
         <div className="list-contacts-top">
@@ -35,6 +36,16 @@ class ListContacts extends Component {
             onChange={(event) => this.updateQuery(event.target.value)}
           />
         </div>
+
+        {showingContacts.length !== contacts.length && (
+          <div className="showing-contacts">
+            <span>
+              Now showing {showingContacts.length} of {contacts.length}
+            </span>
+            <button onClick={this.clearQuery}>Show all</button>
+          </div>
+        )}
+
         <ol className="contact-list">
           {showingContacts.map((contact) => (
             <li key={contact.id} className="contact-list-item">
